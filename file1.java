6be4d97ed06af6a39eea06b26156dfedbb1ff713
ratnakar <purty.ratnakar678@gmail.com>
2023-12-16 01:45:42 +0530

    Initial commit

---
+++ file1.java
@@ -0,0 +1,10 @@
+class file1
+{
+     public static void main(String[] args)
+    {
+        System.out.println("deploying jenkins project");
+        System.out.println("If anything going wrong");
+        System.out.println("make it correct");
+        System.out.println("ensure..");
+    }
+}
